658cc3c52d461700851771074c127e6451b811a2
Yochay Menachem <5888633+yochayme@users.noreply.github.com>
2022-07-06 11:40:23 +0300

    Update StepDefinitions.java

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -8,4 +8,16 @@ public class StepDefinitions {
         Belly belly = new Belly();
         belly.eat(cukes);
     }
+    
+    
+   @When("I wait {int} hour")
+    public void i_wait_hour(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        
+    }
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        // Write code here that turns the phrase above into concrete actions
+        
+    }
 }
