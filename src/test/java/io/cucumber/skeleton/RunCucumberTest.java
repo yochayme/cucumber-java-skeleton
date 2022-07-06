4dbfa27af2da6d95d99230e94350b30dc7dfa95c
Yochay Menachem <5888633+yochayme@users.noreply.github.com>
2022-07-06 11:49:03 +0300

    Update RunCucumberTest.java

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -1,17 +1,10 @@
 package io.cucumber.skeleton;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("io/cucumber/skeleton")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
 public class RunCucumberTest {
 }
